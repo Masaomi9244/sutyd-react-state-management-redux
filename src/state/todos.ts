9cea8c49c6fde1f25dd96f7462d11fce9d0672e7
Masaomi9244 <mario8masaomiomi@gmail.com>
2022-07-07 18:55:48 +0900

    reducerの作成

---
+++ src/state/todos.ts
@@ -0,0 +1,31 @@
+import { Reducer } from "react";
+import { Todo } from "src/types";
+
+const ADD_TODO = "ADD_TODO";
+
+const addTodo = (text: Todo["text"]) => {
+  return { type: ADD_TODO, payload: { text } } as const;
+};
+
+const TODOS: Todo[] = [
+  { id: 1, text: "foo", isDone: false },
+  { id: 2, text: "bar", isDone: true },
+];
+
+type Action = ReturnType<typeof addTodo>;
+
+const todosReducer: Reducer<Todo[], Action> = (state = TODOS, action) => {
+  switch (action.type) {
+    case ADD_TODO: {
+      const newTodo = {
+        id: state.length + 1,
+        text: action.payload.text,
+        isDone: false,
+      };
+      return [...state, newTodo];
+    }
+
+    default:
+      return state;
+  }
+};
